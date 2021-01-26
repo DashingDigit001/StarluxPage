ae2822746356d160956cbb94e6249e6fc09f1337
Hank <dashing.hank@gmail.com>
2021-01-26 11:11:20 +0800

    init

---
+++ precache-manifest.ad591b0ec37871d8b53dbd203b81ff94.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4256136ad78f2ad0ea2",
+    "url": "/StarluxPage/css/app.3edd9659.css"
+  },
+  {
+    "revision": "45e71a4a6be0415c307c",
+    "url": "/StarluxPage/css/chunk-vendors.64d43e69.css"
+  },
+  {
+    "revision": "d96def72bc0e386abb20c9b6769d60ca",
+    "url": "/StarluxPage/img/114-min.d96def72.jpg"
+  },
+  {
+    "revision": "8ec40007c7a54a578ba737f78de26c59",
+    "url": "/StarluxPage/img/20210115-DSCF9031-min-1.8ec40007.png"
+  },
+  {
+    "revision": "8085aec5cc64c29de38638ab5f89fb49",
+    "url": "/StarluxPage/img/20210115-DSCF9031-min.8085aec5.png"
+  },
+  {
+    "revision": "ef3487e242f34c895c224b75e396b219",
+    "url": "/StarluxPage/img/20210115-DSCF9083-去背-min.ef3487e2.png"
+  },
+  {
+    "revision": "6447c915d6afd2cf558749a25078229c",
+    "url": "/StarluxPage/img/20210115-DSCF9083-去背2-min.6447c915.png"
+  },
+  {
+    "revision": "8f9c7025a7fbbec633ae9791bd04c3ac",
+    "url": "/StarluxPage/img/20210115-DSCF9313-2-min.8f9c7025.png"
+  },
+  {
+    "revision": "fb9de3115cf0e5e3963f8299fc3ed6d8",
+    "url": "/StarluxPage/img/20210115-DSCF9313-min.fb9de311.png"
+  },
+  {
+    "revision": "3458e0d171350be10a9cc8f51896eb9a",
+    "url": "/StarluxPage/img/20210115-DSCF9615-2-min-mb.3458e0d1.png"
+  },
+  {
+    "revision": "e8db49e47c657d2ae07934c5bd425735",
+    "url": "/StarluxPage/img/20210115-DSCF9615-2-min.e8db49e4.png"
+  },
+  {
+    "revision": "d38d75d814f6c0166efce58cba6b0263",
+    "url": "/StarluxPage/img/20210115-DSCF9646-2-min-mb.d38d75d8.png"
+  },
+  {
+    "revision": "289be722a17e6a0d582fcd64e147acdf",
+    "url": "/StarluxPage/img/20210115-DSCF9646-2-min.289be722.png"
+  },
+  {
+    "revision": "f336dbe90cdab2e368d0f53abba3993d",
+    "url": "/StarluxPage/img/all_mask.f336dbe9.png"
+  },
+  {
+    "revision": "7c99f9aca83d5a4ec60c5a96ed4c91df",
+    "url": "/StarluxPage/img/mask-image.7c99f9ac.svg"
+  },
+  {
+    "revision": "4bd3d26abb3f67da90b7cd3bd0a33782",
+    "url": "/StarluxPage/index.html"
+  },
+  {
+    "revision": "2ff1f7fbf13da08504bb",
+    "url": "/StarluxPage/js/about.d4acf6e5.js"
+  },
+  {
+    "revision": "b4256136ad78f2ad0ea2",
+    "url": "/StarluxPage/js/app.dfd55b18.js"
+  },
+  {
+    "revision": "45e71a4a6be0415c307c",
+    "url": "/StarluxPage/js/chunk-vendors.54378bfa.js"
+  },
+  {
+    "revision": "ce3bdfbd7c9dab2221b106cd1a7e1a44",
+    "url": "/StarluxPage/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/StarluxPage/robots.txt"
+  }
+]);
